ba4abe4e1edadd6ef9b0dace7c10069418477c0f
pablinho <p.olmedosaez@gmail.com>
2022-01-06 23:33:22 -0400

    Logramos que la aplicacion lea los datos almacenados localmente

--- components/addTask.js
+++ components/addTask.js
@@ -1,62 +1,54 @@
-import checkComplete from 'checkComplete.js';
-import deleteIcon from 'deleteIcon.js';
+import checkComplete from './checkComplete.js';
+import deleteIcon from './deleteIcon.js';
 
-export const addtask = (evento) =>{
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento)
-    list.appendChild(task);
-  
-  }
-  
-  
- 
-  
-  const createTask = (evento) => {
+export const addTask = (evento) => {
+  evento.preventDefault();
 
-    evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-    //accede al local storage, recibe como parametro el nombre de la llave que buscamos
-    //Las lineas || permiten que, en caso de obtener null desde el metodo, generen un arreglo vacio, así podemos seguir haciendo push
-    //JSON parse toma el formato string con elo que lo habiamos guardado previamente, y lo retorna como un objeto que si puede leer HTML
+  const list = document.querySelector('[data-list]');
+  const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]'); //Accedemos al elemento hmtl
 
-    const input = document.querySelector('[data-form-input]');
-    const calendar = document.querySelector('[data-form-date]'); //Accedemos al elemento hmtl
-    const date = calendar.value //almacemamos el valor del elemento en una constante
-    const dateFormat = moment(date).format("DD/MM/YYYY") //Convertimos el valor a un formato nuevo con la librería Moment
-    const dateElement = document.createElement("span") //Creamos un elemento span
-    dateElement.innerHTML = dateFormat
-    const value = input.value;
-    
-    const task = document.createElement('li');
-    task.classList.add('card');
-    input.value = '';
-    //backticks
-    const taskContent = document.createElement('div');
-    const taskObj = { //creamos un objeto que almacenara los valores de la tarea y la fecha
-      value,
-      dateFormat,
-  
-    }
-  
-    taskList.push(taskObj)
-    //Con el metodo push agregamos los objetos al arreglo, mas abajo mandamos taskList en vez de taskObj
-  
-  
-    localStorage.setItem("tasks", JSON.stringify(taskList))
-    //API  de almacenamiento en el navegador, el 1er parametro es el nombre del objeto donde se almacenaran los datos(key), el segundo recibe el objeto en cuestion
-  
-    //Usamos la librería JSON y el metodo stringfy, ya que ambos parametros del metodo setitem deben estar en formato String
-  
-    const titleTask = document.createElement('span');
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    // task.innerHTML = content;
-  
-    task.appendChild(taskContent);
-    task.appendChild(dateElement); //Agregamos span a la estructura
-    task.appendChild(deleteIcon());
-    return task
-    
+  const value = input.value;
+  const date = calendar.value; //almacemamos el valor del elemento en una constante
+  const dateFormat = moment(date).format('DD/MM/YYYY'); //Convertimos el valor a un formato nuevo con la librería Moment
+
+  input.value = '';
+  calendar.value = '';
+
+  const taskObj = {
+    value,
+    dateFormat,
   };
+  //creamos un objeto que almacenara los valores de la tarea y la fecha
+
+  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+  //accede al local storage, recibe como parametro el nombre de la llave que buscamos
+  //Las lineas || permiten que, en caso de obtener null desde el metodo, generen un arreglo vacio, así podemos seguir haciendo push
+  //JSON parse toma el formato string con elo que lo habiamos guardado previamente, y lo retorna como un objeto que si puede leer HTML
+  taskList.push(taskObj);
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+
+  const task = createTask(taskObj); //createtask ahora recibe la lista almacenada como parametro
+  list.appendChild(task); //selecciona la lista y añade el elemento
+};
+
+export const createTask = ({ value, dateFormat }) => {
+  const task = document.createElement('li');
+  task.classList.add('card');
+
+  const taskContent = document.createElement('div');
+
+  const titleTask = document.createElement('span'); //Creamos un elemento span
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
+
+  const dateElement = document.createElement('span');
+  dateElement.innerHTML = dateFormat;
+
+  task.appendChild(taskContent);
+  task.appendChild(dateElement);
+  task.appendChild(deleteIcon());
+  return task;
+};
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-import { addtask } from "./components/addTask.js";
+import { addTask } from './components/addTask.js';
+import { readTasks } from './components/readTasks.js';
+
 const btn = document.querySelector('[data-form-btn]');
 
-
-
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', addtask);
+btn.addEventListener('click', addTask);
+
+readTasks();
